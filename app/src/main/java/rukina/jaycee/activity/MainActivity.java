403623d1d0e14feb3e59d77b891cbaff5dc7408d
nandhakumar30 <datacrawl.nandha@gmail.com>
2016-06-04 07:50:20 +0530

    Initial commit

---
+++ app/src/main/java/rukina/jaycee/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package rukina.jaycee.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rukina.jaycee.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
